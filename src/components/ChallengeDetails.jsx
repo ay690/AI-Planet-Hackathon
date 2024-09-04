4f2ff9151531683c1853bab4ac786046bf51b125
Aniket Yadav <aniketyadav690@gmail.com>
2024-09-04 20:52:07 +0530

    fixed: text justification

--- src/components/ChallengeDetails.jsx
+++ src/components/ChallengeDetails.jsx
@@ -51,20 +51,26 @@ const ChallengeDetails = () => {
               </button>
             </div>
           </nav>
-          <p className="font-poppins font-[500] text-[18px] text-gray-500 max-w-full whitespace-pre-line">
-            Butterflies are the adult flying stage of certain insects belonging
-            to an order or group called Lepidoptera. The word "Lepidoptera"
-            means "scaly wings" in Greek. This name perfectly suits the insects
-            in this group because their wings are covered with thousands of tiny
-            scales overlapping in rows. <br className="mt-4" />
-            An agency of the Governmental Wildlife Conservation is planning to
-            implement an automated system based on computer vision so that it
-            can identify butterflies based on captured images. As a consultant
-            for this project, you are responsible for developing an efficient
-            model. <br className="mt-4" />
-            Your Task is to build an Image Classification Model using CNN that
-            classifies to which class of weather each image belongs to.
-          </p>
+          <div className="w-full space-y-4 whitespace-break-spaces">
+            <p className="font-poppins font-[500] text-[18px] text-gray-500 leading-normal text-justify">
+              Butterflies are the adult flying stage of certain insects
+              belonging to an order or group called Lepidoptera. The word
+              "Lepidoptera" means "scaly wings" in Greek. This name perfectly
+              suits the insects in this group because their wings are covered
+              with thousands of tiny scales overlapping in rows.
+            </p>
+            <p className="font-poppins font-[500] text-[18px] text-gray-500 leading-normal text-justify">
+              An agency of the Governmental Wildlife Conservation is planning to
+              implement an automated system based on computer vision so that it
+              can identify butterflies based on captured images. As a consultant
+              for this project, you are responsible for developing an efficient
+              model.
+            </p>
+            <p className="font-poppins font-[500] text-[18px] text-gray-500 leading-normal text-justify">
+              Your Task is to build an Image Classification Model using CNN that
+              classifies to which class of weather each image belongs to.
+            </p>
+          </div>
         </div>
       </div>
     </section>
